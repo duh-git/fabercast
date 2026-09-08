73e69fbc7be1a4a6cc4ff4f6ec34ccfa8e5609da
duh-git <cherebeevkarim2015@gmail.com>
2026-09-08 15:28:01 +0300

    add(Main page): Numbers section

---
+++ src/pages/Main/Numbers/Numbers.jsx
@@ -0,0 +1,10 @@
+import "./Numbers.css"
+
+export default function Numbers() {
+  return <section className="numbers">
+    <p><span>46</span> лет <br /> на рынке</p>
+    <p><span>1200+</span> <br /> реализованных <br /> проектов</p>
+    <p><span>38</span> мастеров <br /> в производстве</p>
+    <p><span>100%</span> <br /> натуральные <br /> материалы</p>
+  </section>
+}

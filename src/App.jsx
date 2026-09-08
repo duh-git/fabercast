dd815ccd9be96f0355cfa3d71d70b24c1643ff52
duh-git <cherebeevkarim2015@gmail.com>
2026-09-09 01:05:16 +0300

    add(package): react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import { Main } from "./pages"
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Main, Catalog } from "./pages";
+import { Header } from "./components";
+import './App.css';
 
 export default function App() {
-  return <>
-    <Main />
-  </>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }

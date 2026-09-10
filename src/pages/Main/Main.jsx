f07d026a3495a8705d46dd10ce4acdd171473f23
duh-git <cherebeevkarim2015@gmail.com>
2026-09-10 08:34:49 +0300

    mobile: Advantages

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -14,7 +14,7 @@ export default function Main() {
     <Gallery />
     {/* <Inspiration /> */}
     <About />
-    {/* <Advantages /> */}
+    <Advantages />
     {/* <Contact /> */}
     {/* <Footer /> */}
   </>
